7139fd173d410e57b241e39f3ce985cd26924368
Anton Boksha <antonboksha@gmail.com>
2018-03-13 21:35:15 +0200

    add shord description about the dataset Imputer

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -15,7 +15,10 @@ x = dataset.iloc[:, :-1].values
 # for the current dataset it's "Purchased" column
 y = dataset.iloc[:, 3].values
 
-
+# Transformer for filling the missing data
+# missing_values - placeholder for the missing data
+# strategy - logic for filling, mean by axis
+# axis - 0 for the column, 1 for the row
 imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
