042435043936897a17ae1851c48f8b75bb119e51
Anton Boksha <antonboksha@gmail.com>
2018-03-15 14:27:00 +0200

    add split the dataset to train and test

--- data_preprocessing_template.py
+++ data_preprocessing_template.py
@@ -2,8 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.preprocessing import Imputer, LabelEncoder, OneHotEncoder
+from sklearn.model_selection import train_test_split
 
-# import the dataset from .csv file
+# Import the dataset from .csv file
 dataset = pd.read_csv('data.csv')
 
 # x is features or independent variables,
@@ -23,7 +24,7 @@ imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)
 imputer = imputer.fit(x[:, 1:3])
 x[:, 1:3] = imputer.transform(x[:, 1:3])
 
-# encoding the categorical data in the dataset
+# Encoding the categorical data in the dataset
 # first column - "Country", because has only 3 possible values (France/Germany/Spain)
 # encoding mean that we transform our possible values(strings) into possible values(numbers)
 # France - 0 / Germany - 1 / Spain - 2
@@ -35,8 +36,14 @@ x[:, 0] = label_encoder_x.fit_transform(x[:, 0])
 one_hot_encoder = OneHotEncoder(categorical_features=[0])
 x = one_hot_encoder.fit_transform(x).toarray()
 
-
-# encoding the categorical data in the dataset
+# Encoding the categorical data in the dataset
 # so our "Purchased" column with Yes/No values will be 1/0
 label_encoder_y = LabelEncoder()
 y = label_encoder_x.fit_transform(y)
+
+# Split our dataset to training and test sets
+# test_size - declaring what part of our dataset we will use as test data in percents
+# So if we have 100 objects in our dataset and test_size 0.2 we will get the 20 objects for test and 80 as training
+# good values 0.2-0.25
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=0)
